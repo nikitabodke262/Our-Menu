e383f006195fd5e7a56358955b91f2d72bdaac8e
Nikita Bodke <nikitabodke262@gmail.com>
2022-05-08 23:42:41 +0530

    Final

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ const menu = [
     {
       id: 1,
       title: 'buttermilk pancakes',
-      category: 'breakfast',
+      category: 'dinner',
       price: 15.99,
       img: './images/item-1.jpeg',
       desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
